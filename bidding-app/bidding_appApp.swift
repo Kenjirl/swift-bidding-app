492e409fc463a3fb3a14aad3cfedb1ea60b78d8e
Kencong <kenjiro442@gmail.com>
2026-04-17 14:30:12 +0800

    Initial Commit

---
+++ bidding-app/bidding_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  bidding_appApp.swift
+//  bidding-app
+//
+//  Created by Benedict Kenjiro Lehot on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct bidding_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
